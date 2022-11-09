1c783fcd67032a23ce1fd0b7598f0d3e2839fcb7
numangit <abdulnumanjob@gmail.com>
2022-11-10 04:26:27 +0600

    home page banner and lastest service added

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Lottie from "lottie-react";
 import camera from './56718-camera.json';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
@@ -12,6 +13,32 @@ const Home = () => {
                     <hr />
                     <p className='fs-4 mt-3 mt-sm-4'>Capture every beautiful moments.</p>
                 </div>
+            </div>
+            <hr className="text-white display-5 mt-0 mt-lg-5" />
+            <h2 className="text-center text-white fs-2 fw-bold"> Latest Services</h2>
+            <hr className="text-white display-5" />
+
+            <div className="d-flex my-2 my-lg-4 col-11 col-lg-9 col-10 mx-auto  bg-glass-dark text-white p-2 rounded-3">
+                <img className="w-25 m-2 rounded" src="https://photographylife.com/wp-content/uploads/2010/09/Nikon-85mm-f1.4G-Image-Sample-31-1536x1025.jpg" alt="Card image" />
+                <div className='mx-auto p-0 p-lg-2'>
+                    <p className="fs-4 my-1 py-0 fw-bold">Wedding Photography</p>
+                    <hr className="d-none d-sm-block my-2" />
+                    <p className="d-none d-sm-block my-1 py-0 text-start">One of the most important events in many people’s lives is a wedding, and that means wedding photographers have a huge responsibility. Not only do wedding photographers need to have good camera and lighting skills, but they also must know how to work with people and capture the right emotions almost effortlessly. This is a difficult type of photography to practice, and you only get one chance to do it right, so I have a lot of respect for good wedding photographers.</p>
+                    <div className="d-lg-flex">
+                        <span className="d-none d-sm-block py-0 me-auto">
+                            <p className="my-2 py-0 " ><small><span className="fw-semibold text-muted">NIKON D800E +</span></small></p>
+                        </span>
+                        <span className='ms-auto pt-3 pt-lg-0'>
+                            <Link className="my-2 py-0"
+                                to='/'>
+                                <button className="btn bg-white btn-sm fw-bolder">Details</button>
+                            </Link>
+                        </span>
+                    </div>
+                </div>
+                <hr className="text-white display-5 mt-0 mt-lg-5" />
+                <h2 className="text-center text-white fs-2 fw-bold"> Add your suggestion</h2>
+                <hr className="text-white display-5" />
             </div>
         </div >
     );
