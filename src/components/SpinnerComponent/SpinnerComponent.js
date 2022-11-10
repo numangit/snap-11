c72da75a636088a82ec90d1626ce8d71a3e1cc8f
numangit <abdulnumanjob@gmail.com>
2022-11-10 14:00:36 +0600

    spinner component added

--- src/components/SpinnerComponent/SpinnerComponent.js
+++ src/components/SpinnerComponent/SpinnerComponent.js
@@ -3,7 +3,11 @@ import React from 'react';
 const SpinnerComponent = () => {
     return (
         <div>
-            <h1>SPINNER</h1>
+            <div className="text-center">
+                <div className="spinner-border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </div>
+            </div>
         </div>
     );
 };
